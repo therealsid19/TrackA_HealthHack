fb578dc12f3ce3a57e7cc2034ed112fa97034c4d
therealsid19 <siddyboy2003@gmail.com>
2024-07-28 04:07:22 -0400

    Minor changes

--- src/UserPage/UserProfile.js
+++ src/UserPage/UserProfile.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './UserProfile.css';
+import pfp from '../assets/John-pic.png'
 
 const UserProfile = () => {
   return (
     <div className='user-profile'>
-      <img src='https://via.placeholder.com/150' alt='Profile' className='profile-picture' />
+      <img src={pfp} alt='Profile' className='profile-picture' />
       <h3 className='user-name'>John Doe</h3>
     </div>
   );
